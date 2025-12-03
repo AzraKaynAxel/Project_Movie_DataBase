a60410305b40edb7b73ece058fa9523cdf5d4e54
AzraKayn/Axel <axforjob.hay@gmail>
2025-12-03 18:17:50 +0100

    Création des entités

---
+++ src/main/java/fr/diginamic/entites/Acteur.java
@@ -0,0 +1,60 @@
+package fr.diginamic.entites;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinTable;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@Table(name="Acteur")
+public class Acteur extends Personne{
+
+    @Column(name="taille", nullable=false ,precision=3,scale=2)
+    private float taille;
+
+
+    /**
+     * Constructor for: Acteur
+     */
+    public Acteur() {}
+
+    /**
+     * Constructor for: Acteur
+     *
+     * @param id
+     * @param identite
+     * @param dateAnniversaire
+     * @param lieuNaissance
+     * @param pays
+     * @param url
+     * @param taille
+     */
+    public Acteur(String id, String identite, LocalDate dateAnniversaire, String lieuNaissance, Pays pays, String url, float taille) {
+        super(id, identite, dateAnniversaire, lieuNaissance, pays, url);
+        this.taille = taille;
+    }
+
+    /**
+     * Getter for taille
+     *
+     * @return taille
+     */
+    public float getTaille() {
+        return taille;
+    }
+
+    /**
+     * Setter for taille
+     *
+     * @param taille
+     */
+    public void setTaille(float taille) {
+        this.taille = taille;
+    }
+
+    @Override
+    public String toString() {
+        return "Acteur: " + super.toString() + " taille= " + taille;
+    }
+}
---
+++ src/main/java/fr/diginamic/entites/Film.java
@@ -0,0 +1,309 @@
+package fr.diginamic.entites;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name="film")
+public class Film {
+
+    @Id
+    @Column(name="id")
+    private String id;
+
+    @Column(name = "nom", length = 50, nullable = false, unique = true)
+    private String nom;
+
+    @Column(name = "date_sortie", nullable = false)
+    private LocalDate dateSortie;
+
+    @Column(name = "rating", nullable = true, precision=4, scale=2)
+    private float rating;
+
+    @Column(name = "url", length = 255, nullable = true)
+    private String url;
+
+    @Column(name="lieu_tournage", length=50, nullable = false)
+    private String lieuTournage;
+
+    @Column(name="resume", columnDefinition="TEXT", length=255, nullable = false)
+    private String resume;
+
+    @ManyToMany
+    @JoinTable(name="fil_pers", joinColumns=@JoinColumn(name="id_fil", referencedColumnName="id"), inverseJoinColumns=@JoinColumn(name="id_pers", referencedColumnName="id"))
+    private Set<Personne> realisteurs;
+
+    @ManyToMany
+    @JoinTable(name="fil_ger", joinColumns=@JoinColumn(name="id_fil", referencedColumnName="id"), inverseJoinColumns=@JoinColumn(name="id_ger", referencedColumnName="id"))
+    private Set<Genre> genres;
+
+    @ManyToOne
+    @JoinColumn(name="id_pays", nullable=false)
+    private Pays pays;
+
+    @ManyToOne
+    @JoinColumn(name="id_langue", nullable = false)
+    private Langue langue;
+
+    @OneToMany(mappedBy="film")
+    private List<Role> roles = new ArrayList<Role>();
+
+
+    /**
+     * Constructor for: Film
+     */
+    public Film() {}
+
+    /**
+     * Constructor for: Film
+     *
+     * @param id
+     * @param nom
+     * @param dateSortie
+     * @param rating
+     * @param url
+     * @param lieuTournage
+     * @param genres
+     * @param langue
+     * @param resume
+     * @param pays
+     */
+    public Film(String id, String nom, LocalDate dateSortie, float rating, String url, String lieuTournage, Set<Genre> genres, Langue langue, String resume, Pays pays) {
+        this.id = id;
+        this.nom = nom;
+        this.dateSortie = dateSortie;
+        this.rating = rating;
+        this.url = url;
+        this.lieuTournage = lieuTournage;
+        this.genres = genres;
+        this.langue = langue;
+        this.resume = resume;
+        this.pays = pays;
+    }
+
+    /**
+     * Getter for id
+     *
+     * @return id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Setter for id
+     *
+     * @param id
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * Getter for nom
+     *
+     * @return nom
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * Setter for nom
+     *
+     * @param nom
+     */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    /**
+     * Getter for dateSortie
+     *
+     * @return dateSortie
+     */
+    public LocalDate getDateSortie() {
+        return dateSortie;
+    }
+
+    /**
+     * Setter for dateSortie
+     *
+     * @param dateSortie
+     */
+    public void setDateSortie(LocalDate dateSortie) {
+        this.dateSortie = dateSortie;
+    }
+
+    /**
+     * Getter for rating
+     *
+     * @return rating
+     */
+    public float getRating() {
+        return rating;
+    }
+
+    /**
+     * Setter for rating
+     *
+     * @param rating
+     */
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    /**
+     * Getter for url
+     *
+     * @return url
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * Setter for url
+     *
+     * @param url
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    /**
+     * Getter for lieuTournage
+     *
+     * @return lieuTournage
+     */
+    public String getLieuTournage() {
+        return lieuTournage;
+    }
+
+    /**
+     * Setter for lieuTournage
+     *
+     * @param lieuTournage
+     */
+    public void setLieuTournage(String lieuTournage) {
+        this.lieuTournage = lieuTournage;
+    }
+
+    /**
+     * Getter for resume
+     *
+     * @return resume
+     */
+    public String getResume() {
+        return resume;
+    }
+
+    /**
+     * Setter for resume
+     *
+     * @param resume
+     */
+    public void setResume(String resume) {
+        this.resume = resume;
+    }
+
+    /**
+     * Getter for pays
+     *
+     * @return pays
+     */
+    public Pays getPays() {
+        return pays;
+    }
+
+    /**
+     * Setter for pays
+     *
+     * @param pays
+     */
+    public void setPays(Pays pays) {
+        this.pays = pays;
+    }
+
+    /**
+     * Getter for langue
+     *
+     * @return langue
+     */
+    public Langue getLangue() {
+        return langue;
+    }
+
+    /**
+     * Setter for langue
+     *
+     * @param langue
+     */
+    public void setLangue(Langue langue) {
+        this.langue = langue;
+    }
+
+    /**
+     * Getter for roles
+     *
+     * @return roles
+     */
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    /**
+     * Setter for roles
+     *
+     * @param roles
+     */
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
+    /**
+     * Getter for realisteurs
+     *
+     * @return realisteurs
+     */
+    public Set<Personne> getRealisteurs() {
+        return realisteurs;
+    }
+
+    /**
+     * Getter for genres
+     *
+     * @return genres
+     */
+    public Set<Genre> getGenres() {
+        return genres;
+    }
+
+    @Override
+    public String toString() {
+        return "Film: " +
+                "id= '" + id + '\'' +
+                ", nom= '" + nom + '\'' +
+                ", dateSortie= " + dateSortie +
+                ", resume= '" + resume + '\'' +
+                ", genres= " + genres +
+                ", url= '" + url + '\'' +
+                ", rating= " + rating +
+                ", lieuTournage= '" + lieuTournage + '\'' +
+                ", pays= " + pays +
+                ", realisteurs= " + realisteurs +
+                ", roles= " + roles +
+                ", langue= " + langue;
+    }
+}
