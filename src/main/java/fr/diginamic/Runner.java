d609db555caa2fca8389086589a22c3cd3af4efd
AzraKayn/Axel <axforjob.hay@gmail>
2025-12-08 15:47:07 +0100

    Traitement des Genres avec le fichier films.csv + corretive nécessaire pour faire fonctionner les jointures

--- src/main/java/fr/diginamic/Runner.java
+++ src/main/java/fr/diginamic/Runner.java
@@ -9,10 +9,13 @@ public class Runner {
         CsvService csvService = new CsvService();
         DataBaseService dataBaseService = new DataBaseService();
 
-        dataBaseService.connexionDataBase();
+        //dataBaseService.connexionDataBase();
 
-        dataBaseService.insertionDataBase("D:/Cours_CDA/17 - Projet en équipe - développement d'une application JAVA avec JPA/Projet 4 - Internet Movie DataBase - niveau 3/acteurs.csv");
-        dataBaseService.insertionDataBase("D:/Cours_CDA/17 - Projet en équipe - développement d'une application JAVA avec JPA/Projet 4 - Internet Movie DataBase - niveau 3/realisateurs.csv");
+        //dataBaseService.insertionDataBase("D:/Cours_CDA/17 - Projet en équipe - développement d'une application JAVA avec JPA/Projet 4 - Internet Movie DataBase - niveau 3/pays.csv");
+        //dataBaseService.insertionDataBase("D:/Cours_CDA/17 - Projet en équipe - développement d'une application JAVA avec JPA/Projet 4 - Internet Movie DataBase - niveau 3/acteurs.csv");
+        //dataBaseService.insertionDataBase("D:/Cours_CDA/17 - Projet en équipe - développement d'une application JAVA avec JPA/Projet 4 - Internet Movie DataBase - niveau 3/realisateurs.csv");
+
+        csvService.traitementDesGenres("D:/Cours_CDA/17 - Projet en équipe - développement d'une application JAVA avec JPA/Projet 4 - Internet Movie DataBase - niveau 3/films.csv");
 
         //csvService.traitementDesFilms("D:/Cours_CDA/17 - Projet en équipe - développement d'une application JAVA avec JPA/Projet 4 - Internet Movie DataBase - niveau 3/films.csv");
         //csvService.traitementDesActeurs("D:/Cours_CDA/17 - Projet en équipe - développement d'une application JAVA avec JPA/Projet 4 - Internet Movie DataBase - niveau 3/acteurs.csv");
--- src/main/java/fr/diginamic/services/CsvService.java
+++ src/main/java/fr/diginamic/services/CsvService.java
@@ -2,9 +2,15 @@ package fr.diginamic.services;
 
 import fr.diginamic.entites.Acteur;
 import fr.diginamic.entites.Film;
+import fr.diginamic.entites.Genre;
+import fr.diginamic.entites.Langue;
 import fr.diginamic.entites.LieuNaissance;
 import fr.diginamic.entites.Pays;
+import fr.diginamic.entites.Personne;
 
+import javax.crypto.spec.PSource;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -15,15 +21,22 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 
 public class CsvService {
 
     private Path path;
     List<String> lines;
     int iMin = 0; // pour tester les listes petit à petit
-    int iMax = 3;
+    int iMax = 100;
+
+    private static final String REQUETE_RECUP_LIEU_NAISSANCE = "SELECT e FROM LieuNaissance e WHERE e.localisation = :value";
+    private static final String REQUETE_RECUP_PAYS = "SELECT e FROM Pays e WHERE e.nom = :value";
+    private static final String REQUETE_RECUP_GENRE = "SELECT e FROM Genre e WHERE e.nom = :value";
 
     public void lectureDeFichierCSV(String monPath) {
         Path pathOrigin = Paths.get(monPath);
@@ -41,40 +54,58 @@ public class CsvService {
 
     }
 
-    public List<Film> traitementDesFilms(String monPath) {
+    public List<Film> traitementDesFilms(String monPath, EntityManager em) {
         lectureDeFichierCSV(monPath);
 
         // Notre date dans le fichier est ex: 1964 donc pas besoin de faire attention à la case
-        DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy");
+        //DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy");
         List<Film> films = new ArrayList<>();
 
         for (int i = iMin; i < iMax; i++) {
             String[] values = lines.get(i).split(";");
+            String[] tabGenre = values[6].trim().split(",");
 
-            System.out.println(Arrays.toString(values));
+
+            //LieuNaissance lieuNaissanceResult = selectLieuNaissance.getSingleResult();
+
             Film film = new Film();
+
+            for (int j = 0; j < tabGenre.length; j++) {
+                TypedQuery<Genre> selectGenres = em.createQuery(REQUETE_RECUP_GENRE, Genre.class);
+                selectGenres.setParameter("value", tabGenre[j]);
+                Genre genre = selectGenres.getSingleResult();
+                film.getGenres().add(genre);
+            }
+
+            if (!values[3].isEmpty()) {
+                film.setRating(Float.parseFloat(values[3]));
+            }
+
+            TypedQuery<Pays> selectPays = em.createQuery(REQUETE_RECUP_PAYS, Pays.class);
+            selectPays.setParameter("value", values[values.length - 1]);
+            Pays pays = selectPays.getSingleResult();
+
             film.setId(values[0]);
             film.setNom(values[1]);
-            //film.setDateSortie(LocalDate.parse(values[2], pattern));
-            film.setRating(Float.parseFloat(values[3]));
+            film.setDateSortie(values[2]);
             film.setUrl(values[4]);
             film.setLieuTournage(values[5]);
+            film.setResume(values[8]);
+            film.setPays(pays);
 
             films.add(film);
         }
         return films;
     }
 
-
     /**
      * @param monPath
+     * @param em
      * @return
      *
      * Cette méthode à pour objectif de retourner une liste d'intance d'Acteur avec les propriétés
-     * Objectif de na pas avoir de duplication de données.
-     *
      */
-    public List<Acteur> traitementDesActeurs(String monPath) {
+    public List<Acteur> traitementDesActeurs(String monPath, EntityManager em) {
         lectureDeFichierCSV(monPath);
 
         /**
@@ -85,41 +116,75 @@ public class CsvService {
          *
          *  Cei est valable pour une date en comme ex: "March 15 1954", "MARCH 15 1954", "march 15 1954".
          */
-
-        DateTimeFormatter pattern = new DateTimeFormatterBuilder().parseCaseInsensitive().appendPattern("MMMM d yyyy").toFormatter(Locale.ENGLISH);
+        //DateTimeFormatter pattern = new DateTimeFormatterBuilder().parseCaseInsensitive().appendPattern("MMMM d yyyy").toFormatter(Locale.ENGLISH);
 
         List<Acteur> acteurs = new ArrayList<>();
 
         for (int i = iMin; i < iMax; i++) {
             String[] values = lines.get(i).split(";");
-            /*String[] naissance = values[3].split(",");
-            StringBuilder monNouveauLieux = new StringBuilder();
 
-            for (int j = 0; j < naissance.length - 1; j++) {
-                if (j < naissance.length - 2) {
-                    monNouveauLieux.append(naissance[j].trim() + ", ");
-                } else  {
-                    monNouveauLieux.append(naissance[j].trim());
-                }
-            }*/
-
-            String dateString = values[2];
-            LocalDate date = LocalDate.parse(dateString.trim(), pattern);
-            LieuNaissance lieuNaissance = new LieuNaissance();
-            lieuNaissance.setLocalisation(values[3]);
+            TypedQuery<LieuNaissance> selectLieuNaissance = em.createQuery(REQUETE_RECUP_LIEU_NAISSANCE, LieuNaissance.class);
+            selectLieuNaissance.setParameter("value", values[3]);
+            LieuNaissance lieuNaissanceResult = selectLieuNaissance.getSingleResult();
 
 
             Acteur acteur = new Acteur();
 
             acteur.setId(values[0]);
             acteur.setIdentite(values[1]);
-            acteur.setDateAnniversaire(date);
-            acteur.setLieuNaissance(lieuNaissance);
-            acteur.setTaille(Float.parseFloat(values[4].replace(" m", "")));
+            acteur.setDateAnniversaire(values[2]);
+            acteur.setLieuNaissance(lieuNaissanceResult);
+
+            if (!values[4].isEmpty()) {
+                acteur.setTaille(Float.parseFloat(values[4].replace(" m", "")));
+            }
+
             acteur.setUrl(values[5]);
             acteurs.add(acteur);
         }
         return acteurs;
+    }
+
+    /**
+     * @param monPath
+     * @param em
+     * @return
+     *
+     * Cette méthode à pour objectif de retourner une liste d'intance de Réalisateur avec les propriétés
+     */
+    public List<Personne> traitementDesRealisateurs (String monPath, EntityManager em) {
+        lectureDeFichierCSV(monPath);
+
+        /**
+         *  DateTimeFormatterBuilder permet de pouvoir utiliser :
+         *  parseCaseInsensitive() (ceci permert de faire attention à la case sinon risque d'avoir une exception sur le mois de l'année en tout lettre)
+         *  appendPattern prend l'écriture présente dans le doc (attention de prendre la même).
+         *  toFormatter car notre date est en anglaise.
+         *
+         *  Cei est valable pour une date en comme ex: "March 15 1954", "MARCH 15 1954", "march 15 1954".
+         */
+        //DateTimeFormatter pattern = new DateTimeFormatterBuilder().parseCaseInsensitive().appendPattern("MMMM d yyyy").toFormatter(Locale.ENGLISH);
+
+        List<Personne> realisateurs = new ArrayList<>();
+
+        for (int i = iMin; i < iMax; i++) {
+            String[] values = lines.get(i).split(";");
+
+            TypedQuery<LieuNaissance> selectLieuNaissance = em.createQuery(REQUETE_RECUP_LIEU_NAISSANCE, LieuNaissance.class);
+            selectLieuNaissance.setParameter("value", values[3]);
+            LieuNaissance lieuNaissanceResult = selectLieuNaissance.getSingleResult();
+
+
+            Personne personne = new Personne();
+            personne.setId(values[0]);
+            personne.setIdentite(values[1]);
+            personne.setDateAnniversaire(values[2]);
+            personne.setLieuNaissance(lieuNaissanceResult);
+            personne.setUrl(values[4]);
+
+            realisateurs.add(personne);
+        }
+        return realisateurs;
     }
 
     /**
@@ -132,7 +197,7 @@ public class CsvService {
         lectureDeFichierCSV(monPath);
         List<Pays> mesPays = new ArrayList<>();
 
-        for (int i = iMin; i < iMax; i++) {
+        for (int i = iMin; i < lines.size(); i++) {
             String[] values = lines.get(i).split(";");
 
             Pays pays = new Pays();
@@ -164,5 +229,35 @@ public class CsvService {
             lieuNaissances.add(lieuNaissance);
         }
         return lieuNaissances;
+    }
+
+    /**
+     * @param monPath
+     * @return
+     *
+     * Cette méthode à pour objectif de retourner une liste d'intance de Genre avec les propriétés
+     * Utilisation de HashMap pour ne pas avoir de doublons
+     *
+     */
+    public List<Genre> traitementDesGenres(String monPath) {
+        lectureDeFichierCSV(monPath);
+
+        // Permet de faire une liste de Clé : Valeur
+        HashMap<String, Genre> genres = new HashMap<>();
+
+        for (int i = iMin; i < lines.size(); i++) {
+            String[] values = lines.get(i).split(";");
+            String[] tabGenres = values[6].trim().split(",");
+            for (int j = 0; j < tabGenres.length ; j++) {
+                if (!genres.containsKey(tabGenres[j])) {
+                    Genre genre = new Genre();
+                    genre.setNom(tabGenres[j]);
+                    genres.put(tabGenres[j], genre);
+                }
+            }
+        }
+
+        // Pour transformer une HashMap en List
+        return new ArrayList<>(genres.values());
     }
 }
